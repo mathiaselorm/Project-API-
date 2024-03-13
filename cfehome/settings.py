f3bce4c3bbcd108103b289858201a38c1f04c991
👨‍💻 Mathias Elorm <94559474+mathiaselorm@users.noreply.github.com>
2024-03-13 05:56:27 +0000

    update to settings.py

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    'project-api-n4g3.onrender.com', 
+    'localhost',
+]
 
 # Application definition
 INSTALLED_APPS = [
