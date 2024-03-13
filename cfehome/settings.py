616d56486fab2950bb3639b6a01181e280b6c334
👨‍💻 Mathias Elorm <94559474+mathiaselorm@users.noreply.github.com>
2024-03-13 04:05:11 +0000

    follow up commit

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@7d1*a$l#fk23mq-k^xjjgdomn$(9-3pgv#)buv29wenv(2dz8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
 
 
 # Application definition
@@ -103,6 +104,8 @@ DATABASES = {
 }
 
 
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -148,3 +151,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
+
+#postgres://product_api_pnn3_user:c0VnShp3WlMYGbahr7t5TOHQRIDZbUgT@dpg-cnoi147sc6pc73b66bn0-a.oregon-postgres.render.com/product_api_pnn3
